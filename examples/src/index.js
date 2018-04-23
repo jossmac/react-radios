75d52d435a83bf842eed7381c9d188174e1df66a
Joss Mackison <joss.mackison@gmail.com>
2018-04-24 09:19:57 +1000

    replace react-create-app with custom webpack recipe

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
-import { RadioGroup, Radio } from './radios';
+import { render } from 'react-dom';
+import { RadioGroup, Radio } from '../../src';
+
+// styled components
+// ------------------------------
 
 const Container = props => (
   <div
     style={{
+      fontFamily: 'sans-serif',
       marginLeft: 'auto',
       marginRight: 'auto',
       maxWidth: 600,
@@ -13,6 +18,9 @@ const Container = props => (
   />
 );
 const Label = props => <label style={{ padding: 8 }} {...props} />;
+
+// data
+// ------------------------------
 
 const groups = [
   {
@@ -25,7 +33,10 @@ const groups = [
   },
 ];
 
-export default class App extends Component {
+// example
+// ------------------------------
+
+class App extends Component {
   state = { [groups[0].name]: groups[0].items[0] };
 
   handleChange = name => value => {
@@ -39,12 +50,13 @@ export default class App extends Component {
         <h1>React Radios</h1>
         {groups.map(g => (
           <RadioGroup
+            key={g.name}
             name={g.name}
             value={this.state[g.name]}
             onChange={this.handleChange(g.name)}
           >
             {g.items.map(i => (
-              <Label>
+              <Label key={i}>
                 <Radio value={i} /> {i}
               </Label>
             ))}
@@ -54,3 +66,8 @@ export default class App extends Component {
     );
   }
 }
+
+// render
+// ------------------------------
+
+render(<App />, document.getElementById('root'));
